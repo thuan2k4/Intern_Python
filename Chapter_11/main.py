cec426c752607a5db0f8ebf3ff9fd8bd405ef6ae
DQThuan <qthuan1234@gmail.com>
2025-04-12 18:04:44 +0700

    [UPDATE] - use flask_jwt_extended like a middleware

--- Chapter_11/main.py
+++ Chapter_11/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
 from users import users_bp
-from models import User, TokenBlockedList
+from middleware import init_app_jwt, jwt_middleware
 
 def create_app():
     app = Flask(__name__)
@@ -13,52 +13,6 @@ def create_app():
     app.register_blueprint(users_bp, url_prefix='/users')
     
     db.init_app(app)
-    jwt.init_app(app)
-    
-    #load user (use for method current_user)
-    @jwt.user_lookup_loader
-    def user_lookup_callback(_jwt_header, jwt_data):
-        identity = jwt_data['sub'] #query username in jwt
-        return User.query.filter_by(username=identity).one_or_none() #just 1 query
-    
-    #additional claims loader
-    @jwt.additional_claims_loader
-    def add_claims_to_access_token(identity):
-        if identity == 'Carol':
-            return {"is_staff": True}
-        return {"is_staff": False}
-    
-    # jwt error handler
-    
-    #Token hết hạn
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return jsonify({
-            "message": "The token has expired.",
-            "error": "token_expired"
-        }), 401
-    
-    #Token không hợp lệ
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return jsonify({
-            "message": "Signature verification failed.",
-            "error": "invalid_token"
-        }), 401
-    
-    #Không có token
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return jsonify({
-            "message": "Request does not contain an access token.",
-            "error": "authorization_required"
-        }), 401
-    
-    #check if token is revoked
-    @jwt.token_in_blocklist_loader
-    def check_if_token_revoked(jwt_header, jwt_payload):
-        jti = jwt_payload['jti']
-        token = db.session.query(TokenBlockedList).filter(TokenBlockedList.jti == jti).scalar()
-        return token is not None
+    init_app_jwt(app)
     
     return app
---
+++ Chapter_11/middleware.py
@@ -0,0 +1,53 @@
+from extensions import db, jwt
+from flask import request, jsonify
+from models import User, TokenBlockedList
+from functools import wraps
+
+def init_app_jwt(app):
+    jwt.init_app(app)
+    
+    #load user (use for method current_user)
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data['sub'] #query username in jwt
+        return User.query.filter_by(username=identity).one_or_none() #just 1 query
+    
+    #additional claims loader
+    @jwt.additional_claims_loader
+    def add_claims_to_access_token(identity):
+        if identity == 'Carol':
+            return {"is_staff": True}
+        return {"is_staff": False}
+    
+    # jwt error handler
+    
+    #Token hết hạn
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({
+            "message": "The token has expired.",
+            "error": "token_expired"
+        }), 401
+    
+    #Token không hợp lệ
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            "message": "Signature verification failed.",
+            "error": "invalid_token"
+        }), 401
+    
+    #Không có token
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            "message": "Request does not contain an access token.",
+            "error": "authorization_required"
+        }), 401
+    
+    #check if token is revoked
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        jti = jwt_payload['jti']
+        token = db.session.query(TokenBlockedList).filter(TokenBlockedList.jti == jti).scalar()
+        return token is not None
